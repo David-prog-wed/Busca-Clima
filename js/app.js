e35543b0d91577ab394508a5483150e982b99abc
David-prog-wed <dedcelisg@hotmail.es>
2022-07-12 18:59:50 -0500

    Agregando document 'DOMContentLoaded'

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const container = document.querySelector(".container");
 const resultado = document.querySelector("#resultado");
 const formulario = document.querySelector("#formulario");
 
-window.addEventListener("load", () => {
+document.addEventListener("DOMContentLoaded", () => {
   formulario.addEventListener("submit", buscarClima);
 });
 
